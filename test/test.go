dd98c64de295d5c6df29b95dc16bc226b7974f4e
josue eduardo <josue.ferrufino.soft@gmail.com>
2018-11-24 12:52:17 -0400

    obtener todos los archivos con su hash respectivo

--- test/test.go
+++ test/test.go
@@ -1,9 +1,10 @@
-package test
+package main
 
 import "testing"
+import "github.com/josue1471515/APIGolang"
 func TestSum(t *testing.T) {
-	//total := getListFiles("string")
-	////if total != 10 {
-	////	t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
-	////}
+	total := getListFiles("string")
+	//if total != 10 {
+	//	t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	//}
 }
