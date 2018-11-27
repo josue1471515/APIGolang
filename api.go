eae7c38be9e44afeb1a3fa905cd3ac1917061126
josue eduardo <josue.ferrufino.soft@gmail.com>
2018-11-26 20:21:56 -0400

    terminado el API

--- api.go
+++ api.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/gorilla/mux"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func getDocuments(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +16,7 @@ func getDocuments(w http.ResponseWriter, r *http.Request) {
 	var docs []Document
 	docs = getDocumentsTypeList()
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusForbidden)
 	json.NewEncoder(w).Encode(docs)
 
 }
@@ -24,9 +27,11 @@ func getDocumentsById(w http.ResponseWriter, r *http.Request) {
 	log.Println(keys)
 	doc, err := getDocumentByIdMd5(id)
 	if err == nil {
+		w.WriteHeader(http.StatusForbidden)
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(doc)
 	} else {
+		w.WriteHeader(http.StatusInternalServerError)
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(err)
 	}
@@ -48,6 +53,7 @@ func getDocumentsTypeList() []Document {
 				Document{ID: hashMd5, Name: value, Size: fileSize})
 		}
 	}
+
 	return docs
 }
 
@@ -61,4 +67,27 @@ func getDocumentByIdMd5(id string) (Document, error) {
 		}
 	}
 	return Document{}, errors.New("No se encontro el ID ")
+}
+
+func createDocument(writer http.ResponseWriter, request *http.Request) {
+	//request.ParseMultipartForm(32 << 20)
+	file, handler, err := request.FormFile("file")
+	defer file.Close()
+
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		errors.New("No se puede extraer el archivo")
+	}
+	f, err := os.OpenFile("./files/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		errors.New("No se puedo guardar el archivo")
+	}
+
+	defer f.Close()
+	io.Copy(f, file)
+	writer.WriteHeader(http.StatusForbidden)
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode("Se guardo correctamente el archivo " + handler.Filename)
 }
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
 )
 
 var PathDirectory = `./files/`
@@ -12,5 +14,31 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/documents", getDocuments).Methods("GET")
 	router.HandleFunc("/documents/{ID}", getDocumentsById).Methods("GET")
+	router.HandleFunc("/documents", createDocument).Methods("POST")
+	router.HandleFunc("/documents/{ID}", deleteDocument).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":9000", router))
+}
+func deleteDocument(writer http.ResponseWriter, request *http.Request) {
+	keys := mux.Vars(request)
+	id := keys["ID"]
+	var docs []Document
+	docs = getDocumentsTypeList()
+	sw := false
+	docName := ""
+	for _, doc := range docs {
+		if doc.ID == id {
+			sw = true
+			docName = doc.Name
+			os.Remove("./files/" + doc.Name)
+		}
+	}
+	if !sw {
+		writer.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(writer).Encode("No se encontro el archivo ")
+	} else {
+		writer.WriteHeader(http.StatusForbidden)
+		writer.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(writer).Encode("Se Elimino correctamente el archivo " + docName)
+	}
+
 }
